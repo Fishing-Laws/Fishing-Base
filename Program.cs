ac274dcf42dd488e31315613618883174cbd4192
SwallowNest111 <67299607+SwallowNest111@users.noreply.github.com>
2020-06-23 10:13:56 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _3_1
+{
+    class Program
+    {
+        class Array
+        {
+            private int[,] a;
+
+            public Array()
+            {
+                a = new int[4, 4] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+            }
+            public void search(int x)
+            {
+                int i, j;
+
+                for (i = 3, j = 0; i >= 0 && j < 4;)
+                {
+                    if (x == a[i, j])
+                    {
+                        Console.WriteLine("ok");
+                        return;
+                    }
+                    else if (x < a[i, j])
+                    {
+                        i--;
+                        continue;
+                    }
+                    else if (x > a[i, j])
+                    {
+                        j++;
+                        continue;
+                    }
+                }
+            }
+        }
+            
+
+        static void Main(string[] args)
+        {
+            Array a = new Array();
+            a.search(90);
+        }
+    }
+}
